d6cb262588e3fca3ba94dcfc9cbcd0e176762b9e
Ihor Chulinda <ichulinda@gmail.com>
2020-05-18 03:51:36 +0200

    feat: replace chat message with explanation line for party size

--- Patches/DefaultPartySizeLimitModelPatch.cs
+++ Patches/DefaultPartySizeLimitModelPatch.cs
@@ -1,4 +1,4 @@
-﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.SandBox.GameComponents.Party;
 using TaleWorlds.Core;
 using HarmonyLib;
@@ -13,17 +13,13 @@ namespace AttributesReloaded
 		public static int Postfix(int __result, PartyBase party, StatExplainer explanation = null)
 		{
 			int bonus = 0;
-			var showMessages = party.LeaderHero == Hero.MainHero && Config.Instance.enable_messages;
 			if (party.LeaderHero == null) return __result;
 			// if we take all party members into account to calculate bonus, then we don't need to take leader additionally
 			if (!Config.Instance.party_size_from_all_heroes)
 			{
 				var leaderBonuses = new CharacterAttributeBonuses(party.LeaderHero.CharacterObject);
 				bonus += (int)(__result * leaderBonuses.PartySizeMultiplier);
-				if (showMessages)
-				{
-					InformationManager.DisplayMessage(new InformationMessage("Bonus " + bonus + " party size limit from SOC", Colors.Red));
-				}
+                if (explanation != null) explanation.AddLine("Leader's SOC", bonus);
 			}
 			else
 			{
@@ -34,14 +30,12 @@ namespace AttributesReloaded
 						var companionBonuses = new CharacterAttributeBonuses(troop);
 						var companionBonus = (int)(__result * companionBonuses.PartySizeMultiplier); ;
 						bonus += companionBonus;
-						if (showMessages)
-						{
-							InformationManager.DisplayMessage(new InformationMessage("Bonus " + companionBonus + " party size limit from " + troop.Name + "'s SOC", Colors.Red));
-						}
+                        if (explanation != null) explanation.AddLine(troop.Name + "'s SOC", companionBonus);
 					}
 				}
 			}
-			return __result + bonus;
+
+            return __result + bonus;
 		}
 		
 	}
